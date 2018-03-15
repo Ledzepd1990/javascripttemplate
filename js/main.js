23bed4ff9e91c731652f2524287db7d559648cad
Dean gaggero <deangaggero@gmail.com>
2018-03-14 21:32:42 -0700

    week7exercise

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.queryselector("#unique").innerhtml = "header Text"
+
+function getuserdata() {
+  let InputBox=document.querySelector("InputBox")
+  let inputBoxValue =inputBox.value;
+  document.querySelector
+}
